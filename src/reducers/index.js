5f9a4e9fc95004f8169f7c924d2e7e63e825ab0e
Arthur Bonini <arthur.bonini@prs-invivo.com>
2020-08-17 16:04:51 -0400

    add reducer and set up store with thunk

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
 
+import postReducer from "./postsReducer";
+
 export default combineReducers({
-  replaceMe: () => "replace me",
+  posts: postReducer,
 });
